9d48abcd369e04cbfecb1a73339774a84dd32e99
rabbuhl <rabbuhl>
2005-02-09 20:39:43 +0000

    Updated.

--- jqlearn/resources/run/qiterate.sh
+++ jqlearn/resources/run/qiterate.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Iterations is " $3
-qmain.exe -o $1 -e $2 -d -n $3 ttt.xml
+jqlearn.exe -o $1 -e $2 -d -n $3 ttt.xml
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-qmain.exe -t -d -n 5000 ttt.xml >> results.txt
+jqlearn.exe -t -d -n 5000 ttt.xml >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-qmain.exe -g -t -d -n 5000 ttt.xml >> results.txt
+jqlearn.exe -g -t -d -n 5000 ttt.xml >> results.txt
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-qmain.exe -t -n 10 ttt.xml >> results.txt
+jqlearn.exe -t -n 10 ttt.xml >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-qmain.exe -g -t -n 10 ttt.xml >> results.txt
+jqlearn.exe -g -t -n 10 ttt.xml >> results.txt
 
 ##############################
-# qmain.exe -d -n $1 ttt.xml
-# qmain.exe -t -d -n 2500 ttt.xml >> results.txt
-# qmain.exe -t -n 1000 ttt.xml >> results.txt
+# jqlearn.exe -d -n $1 ttt.xml
+# jqlearn.exe -t -d -n 2500 ttt.xml >> results.txt
+# jqlearn.exe -t -n 1000 ttt.xml >> results.txt
