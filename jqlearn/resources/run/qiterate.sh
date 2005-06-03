58cd2183f24ddd6713cadce73307432eb0be9403
rabbuhl <rabbuhl>
2005-06-03 18:36:40 +0000

    Updated.

--- jqlearn/resources/run/qiterate.sh
+++ jqlearn/resources/run/qiterate.sh
@@ -3,14 +3,14 @@ echo "Iterations is " $3
 jqlearn.exe -o $1 -e $2 -d -n $3
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-jqlearn.exe -t -d -n 5000 >> results.txt
+./jqlearn.exe -t -d -n 5000 >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-jqlearn.exe -g -t -d -n 5000 >> results.txt
+./jqlearn.exe -g -t -d -n 5000 >> results.txt
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-jqlearn.exe -t -n 10 >> results.txt
+./jqlearn.exe -t -n 10 >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-jqlearn.exe -g -t -n 10 >> results.txt
+./jqlearn.exe -g -t -n 10 >> results.txt
 
 ##############################
 # jqlearn.exe -d -n $1
