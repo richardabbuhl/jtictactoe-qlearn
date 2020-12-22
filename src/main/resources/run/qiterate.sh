311777ab30cf47fa49aba84dad5677437f2dd8a3
Richard Abbuhl <rabbuhl@hotmail.com>
2020-12-22 22:30:57 +0100

    Update pom.xml.

--- src/main/resources/run/qiterate.sh
+++ src/main/resources/run/qiterate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Iterations is " $3
-java -jar ../target/jqlearn-1.0.0-SNAPSHOT-jar-with-dependencies.jar -o $1 -e $2 -d -n $3
+java -jar ../target/jqlearn-tictactoe-1.0.0-SNAPSHOT-jar-with-dependencies.jar -o $1 -e $2 -d -n $3
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-java -jar ../target/jqlearn-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t -d -n 5000 >> results.txt
+java -jar ../target/jqlearn-tictactoe-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t -d -n 5000 >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-java -jar ../target/jqlearn-1.0.0-SNAPSHOT-jar-with-dependencies.jar -g -t -d -n 5000 >> results.txt
+java -jar ../target/jqlearn-tictactoe-1.0.0-SNAPSHOT-jar-with-dependencies.jar -g -t -d -n 5000 >> results.txt
 
 echo -n "1. A="$1, "E="$2, "" >> results.txt
-java -jar ../target/jqlearn-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t -n 10 >> results.txt
+java -jar ../target/jqlearn-tictactoe-1.0.0-SNAPSHOT-jar-with-dependencies.jar -t -n 10 >> results.txt
 echo -n "2. A="$1, "E="$2, "" >> results.txt
-java -jar ../target/jqlearn-1.0.0-SNAPSHOT-jar-with-dependencies.jar -g -t -n 10 >> results.txt
+java -jar ../target/jqlearn-tictactoe-1.0.0-SNAPSHOT-jar-with-dependencies.jar -g -t -n 10 >> results.txt
 
 ##############################
 # jqlearn.exe -d -n $1
