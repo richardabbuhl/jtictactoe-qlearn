ec1aa3d2a9d42580d899b295caf9b8f264401c91
Richard Abbuhl <rabbuhl@hotmail.com>
2024-11-13 14:34:12 +0100

    Convert classes to Java records where it makes sense
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/richardabbuhl/jtictactoe-qlearn?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/test/java/com/jmentor/jqlearn/QLearnTest.java
+++ src/test/java/com/jmentor/jqlearn/QLearnTest.java
@@ -7,28 +7,28 @@ public class QLearnTest {
 
     @Test
     public void testGetMin() {
-        QLearn qLearn = new QLearn();
+        QLearn qLearn = new QLearn(0.10, 0.00, -1000.0, 1000.0, false, new double[QLearn.getPower(3, 9)], QLearn.getPower(3, 9), new Random(), new MersenneTwister(13));
         double min = qLearn.GetMin();
         assertEquals(1000.0, min);
     }
 
     @Test
     public void testGetMax() {
-        QLearn qLearn = new QLearn();
+        QLearn qLearn = new QLearn(0.10, 0.00, -1000.0, 1000.0, false, new double[QLearn.getPower(3, 9)], QLearn.getPower(3, 9), new Random(), new MersenneTwister(13));
         double max = qLearn.GetMax();
         assertEquals(-1000.0, max);
     }
 
     @Test
     public void testSetAlpha() {
-        QLearn qLearn = new QLearn();
+        QLearn qLearn = new QLearn(0.10, 0.00, -1000.0, 1000.0, false, new double[QLearn.getPower(3, 9)], QLearn.getPower(3, 9), new Random(), new MersenneTwister(13));
         qLearn.setAlpha(0.5);
         assertEquals(0.5, qLearn.getAlpha());
     }
 
     @Test
     public void testSetEpsilon() {
-        QLearn qLearn = new QLearn();
+        QLearn qLearn = new QLearn(0.10, 0.00, -1000.0, 1000.0, false, new double[QLearn.getPower(3, 9)], QLearn.getPower(3, 9), new Random(), new MersenneTwister(13));
         qLearn.setEpsilon(0.1);
         assertEquals(0.1, qLearn.getEpsilon());
     }
